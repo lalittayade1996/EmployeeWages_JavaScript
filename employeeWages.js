67688f3fa81c96e7f0606dce993ee99667cd9ab6
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-30 19:13:30 +0530

    ADDED : Wages Till A Condition Of Total Working Hours And Days Reached For Month

--- employeeWages.js
+++ employeeWages.js
@@ -12,12 +12,20 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 2;
 let empHrs = 0;
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 10;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0
 
 for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
     let empcheck =  Math.floor(Math.random()* 10) % 3;
     empHrs += getWorkingHours(empcheck);
+}
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++
+    let empcheck =  Math.floor(Math.random()* 10) % 3;
+    totalEmpHrs += getWorkingHours(empcheck);
 }
 
 function getWorkingHours(empcheck) {
@@ -33,6 +41,6 @@ function getWorkingHours(empcheck) {
 
 empcheck = Math.floor(Math.random() * 10) % 3;
 empHrs = getWorkingHours(empcheck);
-let empWage = empHrs * WAGE_PER_HOUR;
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("Employee Wage:" +empWage);
-console.log("Total Hrs : " +empHrs +" Employee Wage:" +empWage);
+console.log("Total Days :- " + totalWorkingDays +"; total Emp Hrs :- " + totalEmpHrs +"; Employee Wage :- " + empWage);
