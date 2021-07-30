67ca90e67ab9fc1a896de7f8f3d47222cc9a5115
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-30 18:15:19 +0530

    ADDED : Checking Employee Is Present Or Absent

--- employeeWages.js
+++ employeeWages.js
@@ -1,1 +1,10 @@
-console.log("Welcome To Employee Wages Program");
+console.log("-------Welcome To Employee Wages-------");
+const IS_ABSENT = 0;
+
+let empcheck = Math.floor(Math.random() * 10) % 2;
+if(empcheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+}else {
+    console.log("Employee is Present");
+}
